33d7e2276bfe66c9e56f49e63d47ea7625be382a
Amel <amel.cenanovic@yahoo.com>
2017-10-11 23:47:30 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,59 +1,115 @@
-#include <assert.h>
-#include <algorithm>
 #include <iostream>
+#include <fstream>
 #include <time.h>
-#include <list>
 #include <vector>
+#include <string>
 
-#include "dlist.h"
 #include "llist.h"
+#include "dllist.h"
 
 #define ARRSIZE 1000
 
-template <class Iter>
-void selectionSort(Iter, Iter);	// implement
-
-int main(int argc, char** argv) {
-	// generate random array
-	srand (time(NULL));
-	int arr[ARRSIZE];
-	for (int i = 0; i < ARRSIZE; i++) {
-		arr[i] = rand() % 100;
-	}
-
-	// vector
-	std::vector<int> vec, vec2;
-	for (int i = 0; i < ARRSIZE; i++) vec.push_back(arr[i]);
-	vec2 = vec;
-	selectionSort(vec.begin(), vec.end());
-
-	// singly linked list
-	List<int> lst;
-	for (int i = 0; i < ARRSIZE; i++) lst.push_front(arr[i]);
-	selectionSort(lst.begin(), lst.end());
-
-	// doubly linked list
-	DList<int> dlst;
-	for (int i = 0; i < ARRSIZE; i++) dlst.push_front(arr[i]);
-	selectionSort(dlst.begin(), dlst.end());
-
-	// check sort
-	std::sort(vec2.begin(), vec2.end());
-	assert(vec==vec2);
-
-	std::list<int> stlist;
-	for (int i = 0; i < ARRSIZE; i++) { stlist.push_front(arr[i]); }
-	stlist.sort();
-	auto dit = dlst.begin();
-	auto stlit = stlist.begin();
-	while (dit != dlst.end() && stlit != stlist.end()) {
-		assert(*dit == *stlit);
-		++dit; ++stlit;
+template <class Iter, class T>
+void sort(Iter begin, Iter end, T& x){
+	for(Iter i = begin; i != end; ++i){
+		Iter smallest = i;
+		for(Iter j = i; j != end; ++j){
+			if(*j < *smallest) smallest = j;
+		}
+		T temp = *i;
+		*i = *smallest;
+		*smallest = temp;
 	}
 }
 
-// TODO: fill in with selection sort implementation
 template <class Iter>
-void selectionSort(Iter begin, Iter end) {
+void selectionSort(Iter begin, Iter end){
+	sort(begin, end, *begin);
+}
+
+template <class Container, class T, class Iter>
+void insert(Container& c, const T& val, Iter it){
+	for(Iter i = c.begin(); i != c.end(); ++i){
+		if(*i == val){
+			c.insert(++i, val);
+			break;
+		}
+	}
+}
+
+template <class Container, class T>
+void insertNextToMatch(Container &c, const T& val){
+	insert(c, val, c.begin());
+}
+
+struct time {
+	uint64 start;
+	std::string name;
+	time(const std::string& str="") : start(GetTimeMs64()), name(str) {}
+	void getTimeElapsed() { 
+		std::cout << name << "... " << GetTimeMs64() - start << std::endl; 
+	}
+	void startAgain(const std::string& str="") {
+		start = GetTimeMs64();
+		name = str;
+	}
+};
+
+int main(int argc, char** argv) {
+	if (argc != 2) { 
+		std::cout << "Usage: ./a.out <FILENAME>\n";
+		return 0;
+	}
+	std::ifstream fin;
+	fin.open(argv[1]);
 	
+	std::vector<int> v1, v2;
+	int x;
+	while (!fin.eof()) {
+		fin>>x;
+		v1.push_back(x);
+		if (v1.size() % 5 > 2) v2.push_back(x);
+	}
+
+        struct time counter("filling list");
+            List<int> lst;
+        for (int i = 0; i < v1.size(); i++) {	lst.push_front(v1[i]); }
+            counter.getTimeElapsed();
+
+        counter.startAgain("filling dlist");
+            DList<int> dlst;
+        for (int i = 0; i < v1.size(); i++) {	dlst.push_front(v1[i]); }
+            counter.getTimeElapsed();
+
+        counter.startAgain("filling vector");
+            std::vector<int> v;
+        for (int i = 0; i < v1.size(); i++) { v.push_back(v1[i]); }
+            counter.getTimeElapsed();
+    
+        counter.startAgain("sorting list");
+            List<int>::iterator it(lst.begin());
+        for(int i = 0; i<v1.size()-1; i++) ++it;
+            selectionSort(lst.begin(), it);
+        counter.getTimeElapsed();
+
+        counter.startAgain("sorting dlist");
+            selectionSort(dlst.begin(), dlst.end());
+        counter.getTimeElapsed();
+
+        counter.startAgain("sorting vector");
+            selectionSort(v.begin(), v.end());
+        counter.getTimeElapsed();
+
+        counter.startAgain("adding more to list");
+            for(int i = 0; i < v2.size(); i++) { insertNextToMatch(lst, v2[i]); }
+        counter.getTimeElapsed();
+
+        counter.startAgain("adding more to dlist");
+            for(int i = 0; i < v2.size(); i++) { insertNextToMatch(dlst, v2[i]); }
+        counter.getTimeElapsed();
+
+        counter.startAgain("adding more to vector");
+            for(int i = 0; i < v2.size(); i++) { insertNextToMatch(v, v2[i]); }
+        counter.getTimeElapsed();
+        fin.close();
 }
